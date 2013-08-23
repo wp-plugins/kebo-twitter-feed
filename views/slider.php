f1665fdde0c10c5db2c6eed1dab26af2c86cae9a
PeterBooker <PeterBooker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-23 11:54:33 +0000

    Tagging version 0.5.8
    git-svn-id: https://plugins.svn.wordpress.org/kebo-twitter-feed/tags/0.5.8@761204 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/slider.php
+++ views/slider.php
@@ -9,13 +9,17 @@ wp_enqueue_script('jquery');
 kebo_twitter_print_js();
 ?>
 
-<?php $classes = 'kebo-tweets slider ' . $instance['theme']; ?>
+<?php
+$classes[] = 'kebo-tweets';
+$classes[] = 'slider';
+$classes[] = $instance['theme'];
+?>
 
-<ul class="<?php echo $classes; ?>" id="kebo-tweet-slider" data-timer="10000" data-transition="1000" data-animation="fade">
+<ul class="<?php echo implode(' ', $classes); ?>" id="kebo-tweet-slider" data-timer="10000" data-transition="1000" data-animation="fade">
 
     <?php $i = 0; ?>
         
-    <?php if ( isset($tweets[0]->created_at ) ) : ?>
+    <?php if ( isset( $tweets[0]->created_at ) ) : ?>
         
         <?php foreach ($tweets as $tweet) : ?>
 
@@ -29,26 +33,28 @@ kebo_twitter_print_js();
                 }
             ?>
 
-            <li class="tweet">
+            <li class="ktweet">
 
-                <div class="meta">
-                    <a class="account" href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>" target="_blank">@<?php echo $tweet->user->screen_name; ?></a>
-                    <a class="date" href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>/statuses/<?php echo $tweet->id_str; ?>" target="_blank">
+                <div class="kmeta">
+                    <a class="kaccount" href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>" target="_blank">@<?php echo $tweet->user->screen_name; ?></a>
+                    <a class="kdate" href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>/statuses/<?php echo $tweet->id_str; ?>" target="_blank">
                         <time title="<?php _e('Time posted'); ?>: <?php echo date('dS M Y H:i:s e', strtotime($tweet->created_at)); ?>" datetime="<?php echo date('c', strtotime($tweet->created_at)); ?>" aria-label="<?php _e('Posted on '); ?><?php echo date('jS M', strtotime($tweet->created_at)); ?>"><?php echo $created; ?></time>
                     </a>
                 </div>
 
-                <p class="text">
+                <p class="ktext">
                     <?php if ( 'avatar' == $instance['avatar'] ) : ?>
-                        <img class="avatar" src="<?php echo $tweet->user->profile_image_url; ?>" />
+                        <a href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>" target="_blank">
+                            <img class="kavatar" src="<?php echo $tweet->user->profile_image_url; ?>" />
+                        </a>
                     <?php endif; ?>
                     <?php echo $tweet->text; ?>
                 </p>
 
-                <div class="footer">
-                    <a class="reply" title="<?php _e('Reply'); ?>" href="javascript:void(window.open('https://twitter.com/intent/tweet?in_reply_to=<?php echo $tweet->id_str; ?>', 'twitter', 'width=600, height=400'));"></a>
-                    <a class="retweet" title="<?php _e('Re-Tweet'); ?>" href="javascript:void(window.open('https://twitter.com/intent/retweet?tweet_id=<?php echo $tweet->id_str; ?>', 'twitter', 'width=600, height=400'));"></a>
-                    <a class="favourite" title="<?php _e('Favourite'); ?>" href="javascript:void(window.open('https://twitter.com/intent/favorite?tweet_id=<?php echo $tweet->id_str; ?>', 'twitter', 'width=600, height=400'));"></a>
+                <div class="kfooter">
+                    <a class="kreply" title="<?php _e('Reply'); ?>" href="javascript:void(window.open('https://twitter.com/intent/tweet?in_reply_to=<?php echo $tweet->id_str; ?>', 'twitter', 'width=600, height=400'));"></a>
+                    <a class="kretweet" title="<?php _e('Re-Tweet'); ?>" href="javascript:void(window.open('https://twitter.com/intent/retweet?tweet_id=<?php echo $tweet->id_str; ?>', 'twitter', 'width=600, height=400'));"></a>
+                    <a class="kfavourite" title="<?php _e('Favourite'); ?>" href="javascript:void(window.open('https://twitter.com/intent/favorite?tweet_id=<?php echo $tweet->id_str; ?>', 'twitter', 'width=600, height=400'));"></a>
                 </div>
 
             </li>
@@ -56,7 +62,7 @@ kebo_twitter_print_js();
             <?php if ( ++$i == $instance['count'] ) break; ?>
 
         <?php endforeach; ?>
-        
+    
     <?php else : ?>
             
             <p><?php _e('Sorry, no Tweets were found.', 'kebo_twitter'); ?></p>
