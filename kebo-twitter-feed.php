02743d3648e11fa678fb0dddea4d8719c5333be2
PeterBooker <PeterBooker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-26 15:44:01 +0000

    Updated to version 0.9.7
    git-svn-id: https://plugins.svn.wordpress.org/kebo-twitter-feed/trunk@778855 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kebo-twitter-feed.php
+++ kebo-twitter-feed.php
@@ -3,7 +3,7 @@
  * Plugin Name: Kebo - Twitter Feed
  * Plugin URI: http://wordpress.org/plugins/kebo-twitter-feed/
  * Description: Connect your site to your Twitter account and display your Twitter Feed on your website effortlessly with a custom widget. 
- * Version: 0.9.6
+ * Version: 0.9.7
  * Author: Kebo
  * Author URI: http://kebopowered.com
  * Text Domain: kebo_twitter
@@ -15,7 +15,7 @@ if (!defined('ABSPATH'))
     exit;
 
 if (!defined('KEBO_TWITTER_PLUGIN_VERSION'))
-    define('KEBO_TWITTER_PLUGIN_VERSION', '0.9.6');
+    define('KEBO_TWITTER_PLUGIN_VERSION', '0.9.7');
 if (!defined('KEBO_TWITTER_PLUGIN_URL'))
     define('KEBO_TWITTER_PLUGIN_URL', plugin_dir_url(__FILE__));
 if (!defined('KEBO_TWITTER_PLUGIN_PATH'))
@@ -51,7 +51,7 @@ function kebo_twitter_plugin_setup() {
     /**
      * Load Text Domain for Translations.
      */
-    load_plugin_textdomain( 'kebo_twitter', false, KEBO_TWITTER_PLUGIN_PATH . 'languages/' );
+    load_plugin_textdomain( 'kebo_twitter', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
     
 }
 add_action('plugins_loaded', 'kebo_twitter_plugin_setup', 15);
