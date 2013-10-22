539b6f41e6f4fd8f5567cea8ac96add20297b820
PeterBooker <PeterBooker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-22 15:28:51 +0000

    Tagging version 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/kebo-twitter-feed/tags/1.1.3@791835 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/slider.php
+++ views/slider.php
@@ -2,11 +2,6 @@
 /*
  * HTML output for the Horizontal Slider style of widget.
  */
-
-// Requires jQuery
-wp_enqueue_script('jquery');
-// Prints Slider Javascript using 'wp_footer' hook.
-kebo_twitter_print_js();
 ?>
 
 <?php
@@ -66,29 +61,40 @@ if ( is_rtl() ) {
                 $created = date_i18n( $format, strtotime( $tweet->created_at ) );
                     
             }
+            // Prepare Avatar URL
+            if ( is_ssl() ) {
+                
+                $profile_image = ( isset( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->profile_image_url_https : $tweet->user->profile_image_url_https ;
+            
+            } else {
+                
+                $profile_image = ( isset( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->profile_image_url : $tweet->user->profile_image_url ;
+                
+            }
             
             // Check if we should display replies and hide if so and this is a reply.
-            if ( ! true == $instance['conversations'] && ( ! empty( $tweet->in_reply_to_screen_name ) || ! empty( $tweet->in_reply_to_user_id_str ) || ! empty( $tweet->in_reply_to_status_id_str ) ) )
+            if ( ! true == $instance['conversations'] && ( ! empty( $tweet->in_reply_to_screen_name ) || ! empty( $tweet->in_reply_to_user_id_str ) || ! empty( $tweet->in_reply_to_status_id_str ) ) ) {
                 continue; // skip this loop without changing the counter
+            }
             
             ?>
 
-            <li class="ktweet">
+            <li class="ktweet" style="display: none;">
 
                 <div class="kmeta">
-                    <a class="kaccount" href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>" target="_blank">@<?php echo $tweet->user->screen_name; ?></a>
-                    <a class="kdate" href="https://twitter.com/<?php echo $tweet->user->screen_name; ?>/statuses/<?php echo $tweet->id_str; ?>" target="_blank">
+                    <a class="kaccount" href="https://twitter.com/<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->screen_name : $tweet->user->screen_name ; ?>" target="_blank">@<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->screen_name : $tweet->user->screen_name ; ?></a>
+                    <a class="kdate" href="https://twitter.com/<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->screen_name : $tweet->user->screen_name ; ?>/statuses/<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->id_str : $tweet->id_str ; ?>" target="_blank">
                         <time title="<?php _e( 'Time posted', 'kebo_twitter' ); ?>: <?php echo date_i18n( 'dS M Y H:i:s', strtotime( $tweet->created_at ) + $tweet->user->utc_offset ); ?>" datetime="<?php echo date_i18n( 'c', strtotime( $tweet->created_at ) + $tweet->user->utc_offset ); ?>" aria-label="<?php _e('Posted on ', 'kebo_twitter'); ?><?php echo date_i18n( 'dS M Y H:i:s', strtotime( $tweet->created_at ) + $tweet->user->utc_offset ); ?>"><?php echo $created; ?></time>
                     </a>
                 </div>
 
                 <p class="ktext">
                     <?php if ( 'avatar' == $instance['avatar'] ) : ?>
-                        <a href="https://twitter.com/<?php echo ( isset( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->screen_name : $tweet->user->screen_name ; ?>" target="_blank">
-                            <img class="kavatar" src="<?php echo ( isset( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->profile_image_url : $tweet->user->profile_image_url ; ?>" />
+                        <a href="https://twitter.com/<?php echo ( isset( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->screen_name : $tweet->user->screen_name ; ?>" title="<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->name : $tweet->user->name ; ?> @<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->screen_name : $tweet->user->screen_name ; ?>" target="_blank">
+                            <img class="kavatar" src="<?php echo $profile_image; ?>" alt="<?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->user->name : $tweet->user->name ; ?>" />
                         </a>
                     <?php endif; ?>
-                    <?php echo $tweet->text; ?>
+                    <?php echo ( ! empty( $tweet->retweeted_status ) ) ? $tweet->retweeted_status->text : $tweet->text ; ?>
                 </p>
 
                 <div class="kfooter">
@@ -118,20 +124,3 @@ if ( is_rtl() ) {
     <?php unset( $tweets ); ?>
 
 </ul>
-
-<script type="text/javascript">
-    
-    /*
-     * Capture Show/Hide photo link clicks, then show/hide the photo.
-     */
-    jQuery( '.ktweet .kfooter a:not(.ktogglemedia)' ).click(function(e) {
-    
-        // Prevent Click from Reloading page
-        e.preventDefault();
-        
-        var href = jQuery(this).attr('href');
-        window.open( href, 'twitter', 'width=600, height=400, top=0, left=0');
-
-    });
-
-</script>
