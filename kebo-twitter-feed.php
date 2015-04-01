c0f98ae083836326699ed94982d425749b23a2ef
PeterBooker <PeterBooker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 21:57:29 +0000

    updated to version 1.5.8
    git-svn-id: https://plugins.svn.wordpress.org/kebo-twitter-feed/trunk@1125841 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kebo-twitter-feed.php
+++ kebo-twitter-feed.php
@@ -5,7 +5,7 @@
  * Description: Connect your site to your Twitter account and display your Twitter Feed on your website effortlessly with a custom widget. 
  * Version: 1.5.7
  * Author: Kebo
- * Author URI: https://kebopowered.com/
+ * Author URI: https://www.kebo.io/
  * Text Domain: kebo_twitter
  * Domain Path: /languages
  */
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define('KEBO_TWITTER_PLUGIN_VERSION', '1.5.7');
+define('KEBO_TWITTER_PLUGIN_VERSION', '1.5.8');
 define('KEBO_TWITTER_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('KEBO_TWITTER_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
